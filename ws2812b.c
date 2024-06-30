467412cd233f6636df43609b3ce80fcb7b4e74ca
pl <0x00.pl@gmail.com>
2024-06-30 17:01:42 +0800

    add code

---
+++ ws2812b.c
@@ -0,0 +1,160 @@
+#include "ws2812b.h"
+
+#include "gpiohs.h"
+
+// Change this according to FPIOA, GPIO, and hardware configuration
+#define WS2812B_PIN_CLR()                                                      \
+	{                                                                      \
+		gpiohs->output_val.bits.b4 = 0;                                \
+	}
+
+#define WS2812B_PIN_SET()                                                      \
+	{                                                                      \
+		gpiohs->output_val.bits.b4 = 1;                                \
+	}
+
+// The duration / pulse width depends on the CPU clock frequency!
+static inline void WS2812B_Tx0(void)
+{
+	WS2812B_PIN_SET();
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	WS2812B_PIN_CLR();
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+}
+
+static inline void WS2812B_Tx1(void)
+{
+	WS2812B_PIN_SET();
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	WS2812B_PIN_CLR();
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+	asm volatile("nop");
+}
+
+void WS2812B_TxRes(void)
+{
+	WS2812B_PIN_CLR();
+	for (uint64_t i = 0; i < 1000; i++)
+		asm volatile("nop");
+}
+
+void WS2812B_SetLedRGB(uint8_t r, uint8_t g, uint8_t b)
+{
+	uint64_t frameBuf = (g << 16) | (r << 8) | b;
+
+	for (uint64_t mask = 0x800000; mask > 0; mask >>= 1) {
+		if (frameBuf & mask)
+			WS2812B_Tx1();
+		else
+			WS2812B_Tx0();
+	}
+}
---
+++ ws2812b.h
@@ -0,0 +1,37 @@
+#ifndef _WS2812B_H
+#define _WS2812B_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * LED control:
+ * WS2812B_SetLedRGB(); // First LED (Connected to MCU)
+ * WS2812B_SetLedRGB(); // Second LED
+ * WS2812B_SetLedRGB(); // Third LED (If exist)
+ * WS2812B_TxRes()      // Send reset signal
+ *
+ * WS2812B's protocol:
+ * The data is sent in a sequence containing 24 of those bits – 8 bits for each
+ *color Highest bit first, followed by a low “reset” pulse of at least 50µs. 0
+ *is encoded as: T0H = 0.4uS ToL = 0.85uS (Tolerance 0.15uS) 1 is encoded as:
+ *T1H = 0.85uS T1L = 0.4uS (Tolerance 0.15uS) A valid reset: Hold data line low
+ *for at least 50µs.
+ *
+ *                                                           |-->Update color
+ *                                _________________ | >50uS  |
+ * START_______IDLE_______________|1st LED|2nd
+ *LED|___Reset_______IDLE_____________
+ **/
+
+void WS2812B_SetLedRGB(uint8_t r, uint8_t g, uint8_t b);
+void WS2812B_TxRes(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _WS2812B_H */
